db05bb68b02aa114b8c8b7ee9385884135122583
Hadeer <hadeersalah127@gmail.com>
2023-04-29 20:09:38 +0300

    adding new function

--- component/Verify.js
+++ component/Verify.js
@@ -3,16 +3,27 @@ import myImage from './images/logo_black_font_without_border(o).png'
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
-
+import { useRef } from 'react';
 
 function Verify() {
 
     const { register, formState: { errors }, handleSubmit } = useForm()
 
+    const inputRefs = useRef([]);
+
+
     const onSubmit = (data) => {
         console.log(data)
-    } 
+    }
 
+    const handleInputChange = (index, event) => {
+        const inputLength = event.target.value.length;
+        const maxLength = event.target.maxLength;
+
+        if (inputLength >= maxLength && inputRefs.current[index + 1]) {
+            inputRefs.current[index + 1].focus();
+        }
+    };
 
     return (
         <>
@@ -22,7 +33,7 @@ function Verify() {
                     <Row>
 
                         <Col xs={{ offset: 1, span: 4 }}>
-                            <img src={myImage} alt='Bayo Logo' className='image'/>
+                            <img src={myImage} alt='Bayo Logo' className='image' />
                         </Col>
 
                         <Col xs={{ offset: 1, span: 1 }}>
@@ -40,59 +51,75 @@ function Verify() {
                 <h3 className='verify'>Verify Your Identity</h3>
 
                 <p className='code'>We've sent a code to your configured two-factor authentication code</p>
-               
+
+
+
                 <form id="verifyForm" onSubmit={handleSubmit(onSubmit)}>
                     <Row>
                         <Col xs={6}>
-                            <input type='text' name='digit1' maxLength={1}
-                                {...register('digit1', {
-                                    required: true,
-                                })}
+                            <input
+                                type='text'
+                                name='digit1'
+                                maxLength={1}
+                                {...register('digit1', { required: true })}
                                 className={`hover ${errors.digit1 ? 'border-danger text-danger' : ''}`}
-                                >
-                            </input>
+                                ref={(el) => inputRefs.current[0] = el}
+                                onChange={(e) => handleInputChange(0, e)}
+                            />
 
-
-                            <input type='text' maxLength={1}
+                            <input
+                                type='text'
+                                maxLength={1}
                                 name='digit2'
-                                {...register('digit2', {
-                                    required: true,
-                                })}
+                                {...register('digit2', { required: true })}
                                 className={`hover ${errors.digit2 ? 'border-danger text-danger' : ''}`}
-                                ></input>
+                                ref={(el) => inputRefs.current[1] = el}
+                                onChange={(e) => handleInputChange(1, e)}
+                            />
 
-                            <input type='text' name='digit3' maxLength={1}
-                                {...register('digit3', {
-                                    required: true,
-                                })}
+                            <input
+                                type='text'
+                                name='digit3'
+                                maxLength={1}
+                                {...register('digit3', { required: true })}
                                 className={`hover ${errors.digit3 ? 'border-danger text-danger' : ''}`}
-                                ></input>
-
+                                ref={(el) => inputRefs.current[2] = el}
+                                onChange={(e) => handleInputChange(2, e)}
+                            />
 
                         </Col>
 
 
                         <Col xs={6}>
-                            <input type='text' name='digit4' maxLength={1}
-                                {...register('digit4', {
-                                    required: true,
-                                })}
-                                className={`hover ${errors.digit4 ? 'border-danger text-danger' : ''}`} 
-                                ></input>
+                            <input
+                                type='text'
+                                name='digit4'
+                                maxLength={1}
+                                {...register('digit4', { required: true })}
+                                className={`hover ${errors.digit4 ? 'border-danger text-danger' : ''}`}
+                                ref={(el) => inputRefs.current[3] = el}
+                                onChange={(e) => handleInputChange(3, e)}
+                            />
 
-                            <input type='text' name='digit5' maxLength={1}
-                                {...register('digit5', {
-                                    required: true,
-                                })}
-                                className={`hover ${errors.digit5 ? 'border-danger text-danger' : ''}`} 
-                                ></input>
+                            <input
+                                type='text'
+                                name='digit5'
+                                maxLength={1}
+                                {...register('digit5', { required: true })}
+                                className={`hover ${errors.digit5 ? 'border-danger text-danger' : ''}`}
+                                ref={(el) => inputRefs.current[4] = el}
+                                onChange={(e) => handleInputChange(4, e)}
+                            />
 
-                            <input type='text' name='digit6' maxLength={1}
-                                {...register('digit6', {
-                                    required: true,
-                                })}
-                                className={`hover ${errors.digit6 ? 'border-danger text-danger' : ''}`} 
-                                ></input>
+                            <input
+                                type='text'
+                                name='digit6'
+                                maxLength={1}
+                                {...register('digit6', { required: true })}
+                                className={`hover ${errors.digit6 ? 'border-danger text-danger' : ''}`}
+                                ref={(el) => inputRefs.current[5] = el}
+                                onChange={(e) => handleInputChange(5, e)}
+                            />
 
                         </Col>
 
